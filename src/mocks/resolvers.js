c048cd6ca3ec7cacff1e04dfeb4f5acd0d2b571f
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-04-21 00:44:40 -0700

    add mocked project to resolver

--- src/mocks/resolvers.js
+++ src/mocks/resolvers.js
@@ -9,4 +9,15 @@ const mockProfile = {
   avatar: 'mock url',
 };
 
-export { mockUser, mockProfile };
+const mockProject = {
+  userId: '0',
+  title: 'untitled',
+  bpm: 120,
+  volume: -12,
+  channels: [
+    '{ "id": 0, "type": "synth", "osc": "sine", "steps": [null, null, null, null, null, null, null, null], "volume": -5, "reverb": 0.5 }',
+  ],
+  projectId: '0',
+};
+
+export { mockUser, mockProfile, mockProject };
