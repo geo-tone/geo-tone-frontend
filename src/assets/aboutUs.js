8a36be3f28aaec5af8c55c8737645dd1bc7dbb31
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-04-22 09:55:51 -0700

    added last names to our bios

--- src/assets/aboutUs.js
+++ src/assets/aboutUs.js
@@ -1,6 +1,6 @@
 export const aboutUs = [
   {
-    name: 'Forest',
+    name: 'Forest Heims',
     linkedIn: 'https://www.linkedin.com/in/forestheims/',
     gitHub: 'https://github.com/forestheims',
     bio: 'Full Stack Software Engineer in Portland, OR.',
@@ -8,7 +8,7 @@ export const aboutUs = [
     geoTone: './user/:username',
   },
   {
-    name: 'Michelle',
+    name: 'Michelle Nygren',
     linkedIn: 'https://www.linkedin.com/in/michellenygren/',
     gitHub: 'https://github.com/michellerenehey',
     bio: 'Full Stack Software Engineer in Portland, OR.',
@@ -16,7 +16,7 @@ export const aboutUs = [
     geoTone: './user/:username',
   },
   {
-    name: 'Jordan',
+    name: 'Jordan Laurent',
     linkedIn: 'https://www.linkedin.com/in/jlaurentpdx/',
     gitHub: 'https://github.com/jlaurentpdx',
     bio: 'Full Stack Software Engineer in Portland, OR.',
@@ -24,7 +24,7 @@ export const aboutUs = [
     geoTone: './user/:username',
   },
   {
-    name: 'Minoka',
+    name: 'Minoka Kakizaki',
     linkedIn: 'https://www.linkedin.com/in/minoka-kakizaki/',
     gitHub: 'https://github.com/kakizaki55',
     bio: 'Full Stack Software Engineer in Portland, OR. with a background in audio production and a passion for user-centric UX, who believes in inclusion, accessibility, and test-driven production.',
